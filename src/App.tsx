44e4e2ea8fc17fe5d6dfde1b44e78984f1c16048
stailzsanchez <stailzsanchez@gmail.com>
2023-07-09 11:50:41 +0300

    helpers classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { Route, Routes, Link } from "react-router-dom";
 import { AboutAsync } from "./pages/About/About.async";
 import { MainAsync } from "./pages/Main/Main.async";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div
+      className={classNames("app", { hovered: true, selected: true }, [theme])}
+    >
       <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
