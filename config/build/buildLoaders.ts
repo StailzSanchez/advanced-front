e6b408ef103a615c79c3baf9e532fe625cc724dc
stailzsanchez <stailzsanchez@gmail.com>
2023-07-10 01:17:46 +0300

    L17 add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,6 +7,26 @@ export function buildLoaders({ isDev }: BuildOptions) {
     use: ["@svgr/webpack"],
   };
 
+  const babelLoader = {
+    test: /\.(js|mjs|cjs|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
+
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -42,5 +62,5 @@ export function buildLoaders({ isDev }: BuildOptions) {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader, cssLoader, svgLoader, fileLoader];
+  return [babelLoader, typescriptLoader, cssLoader, svgLoader, fileLoader];
 }
