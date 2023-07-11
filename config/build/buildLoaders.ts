38032ea91388b7e82fe3355a4644f9025db53868
stailzsanchez <stailzsanchez@gmail.com>
2023-07-11 07:21:00 +0300

    eslint

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,24 +1,24 @@
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import { BuildOptions } from "./types/config";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions) {
   const svgLoader = {
     test: /\.svg$/,
-    use: ["@svgr/webpack"],
+    use: ['@svgr/webpack'],
   };
 
   const babelLoader = {
     test: /\.(js|mjs|cjs|jsx|tsx)$/,
     exclude: /node_modules/,
     use: {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
-        presets: ["@babel/preset-env"],
+        presets: ['@babel/preset-env'],
         plugins: [
           [
-            "i18next-extract",
+            'i18next-extract',
             {
-              locales: ["ru", "en"],
+              locales: ['ru', 'en'],
               keyAsDefaultValue: true,
             },
           ],
@@ -30,20 +30,20 @@ export function buildLoaders({ isDev }: BuildOptions) {
   const cssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
-      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       // { loader: "css-modules-typescript-loader" },
       {
-        loader: "css-loader",
+        loader: 'css-loader',
         options: {
           modules: {
-            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
             localIdentName: isDev
-              ? "[name]__[local]--[hash:base64:8]"
-              : "[hash:base64:8]",
+              ? '[name]__[local]--[hash:base64:8]'
+              : '[hash:base64:8]',
           },
         },
       },
-      "sass-loader",
+      'sass-loader',
     ],
   };
 
@@ -51,14 +51,14 @@ export function buildLoaders({ isDev }: BuildOptions) {
     test: /\.(png|jpe?g|gif|woff2|woff)$/i,
     use: [
       {
-        loader: "file-loader",
+        loader: 'file-loader',
       },
     ],
   };
 
   const typescriptLoader = {
     test: /\.tsx?$/,
-    use: "ts-loader",
+    use: 'ts-loader',
     exclude: /node_modules/,
   };
 
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,18 @@
-import React, { Suspense, useContext, useState } from "react";
-import { Route, Routes, Link } from "react-router-dom";
-import "./styles/index.scss";
+import React, { Suspense } from 'react';
+import './styles/index.scss';
 
-import { classNames } from "shared/lib/classNames";
-import { useTheme } from "./providers/ThemeProvider";
-import { About } from "pages/About";
-import { Main } from "pages/Main";
-import { AppRouter } from "./providers/router";
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
-import { useTranslation } from "react-i18next";
-import { LangSwitcher } from "widgets/LangSwitcher";
+import { classNames } from 'shared/lib/classNames';
+import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { AppRouter } from './providers/router';
+import { useTheme } from './providers/ThemeProvider';
 
 export const App = () => {
   const { theme } = useTheme();
 
   return (
     <div
-      className={classNames("app", { hovered: true, selected: true }, [theme])}
+      className={classNames('app', { hovered: true, selected: true }, [theme])}
     >
       <Suspense fallback="">
         <Navbar />
