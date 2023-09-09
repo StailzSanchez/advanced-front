c567b1d47c296aca7b43f1af87502cf101a65e44
stailz <stailzsanchez@gmail.com>
2023-09-09 19:15:52 +0300

    L28 fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,7 +32,7 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     'linebreak-style': [0, 'lf'],
-    'wrap-iife': 'any',
+    'wrap-iife': 'off',
     // 'object-curly-newline': ['error', 'consistent'],
     'max-len': ['warn', { code: 110, ignoreComments: true }],
     'operator-linebreak': ['error', 'before'],
